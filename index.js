6eef9eb5572042e33d954ec5fcde4433afd8b1aa
GreyWoulf <93624560+GreyWoulf@users.noreply.github.com>
2022-04-28 14:54:09 -0400

    started the conditional statements

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Define a function
 
 function cashRegister (price, cash, cid){
-    // Add up all the cash in the cid
-    // start by declaring a new variable and setting it to 0
-    let totalCID = 0;
-    // loop through the cid array
-    // make an array into an object maybe
+    // make the cid array into an object maybe
 //     [
 //         ["PENNY", 1.01],
 //         ["NICKEL", 2.05],
@@ -44,6 +40,42 @@ function cashRegister (price, cash, cid){
             //     TWENTY: 60,
             //     'ONE HUNDRED': 100
             //   } 
+        }
+    }
+    // now we can find the totals for totals for the cash in the drawer
+        // Add up all the cash in the cid
+    // start by declaring a new variable and setting it to 0
+    let totalCID = 0;
+    // now we are going to loop through the object
+    for (let key in obj){
+        // we are going to add each key value to the totalCID variable
+        totalCID += obj[key];
+        // noticed totalCID has long decimal, so we need to round it to 2 decimals
+    }
+    // rounding totalCID to 2 decimals
+    totalCID = totalCID.toFixed(2);
+    // now we are going to create a variable amtDue variable that let's us know the change we need to give
+    let amtDue = cash - price;
+    // first we are going to check if they gave us cash to cover the price
+    if (cash < price){
+        // then we are going to return an object that says status is insufficient funds
+        return {status: "INCORRECT_PAYMENT", change: []}
+    }
+    else {
+        continue;
+    }
+    // now we are going to check to see if the cash is equal to the total we have in the register
+    if (cash === totalCID){
+        // if it is then we are going to return an object that says status is closed
+        return {status: "CLOSED", change: []};
+    }
+    // now we are going to check if cash is less than the totalCID
+    if (cash < totalCID){
+        // then we are going to loop through the object to give them their change
+        // we actually we need to loop backwards so we can have go through the big bills first and then the change
+        for(let key in obj){
+            // while amount due is greater than  
+
         }
     }
 }
