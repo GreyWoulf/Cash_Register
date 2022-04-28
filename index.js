51c5d81410e7d04e23c265ebd1ae4e12abf0302d
GreyWoulf <93624560+GreyWoulf@users.noreply.github.com>
2022-04-28 16:50:17 -0400

    started over because my code kept failing some aspects

--- index.js
+++ index.js
@@ -1,85 +1,30 @@
 // Define a function
 
 function cashRegister (price, cash, cid){
-    // make the cid array into an object maybe
-//     [
-//         ["PENNY", 1.01],
-//         ["NICKEL", 2.05],
-//         ["DIME", 3.1],
-//         ["QUARTER", 4.25]
-//         ["ONE", 90],
-//         ["FIVE", 55],
-//         ["TEN", 20],
-//         ["TWENTY", 60],
-//         ["ONE HUNDRED", 100]
-//       ]
-    // We are going to reverse the array
-    cid = cid.reverse();
-    // Flatten the array
-    cid = cid.flat();
-    // we will create a new empty object
-    let obj = {};
-    // we will now loop through the now flattened array
-    for(let i = 0; i < cid.length; i++){
-        // if the ith variable is even
-        if(i % 2 === 0){
-            // then we are going to convert the even number to values and odd numbers in keys
-            let key = cid[i];
-            let value = cid[i + 1];
-
-            // we are going to assign the key and value to the obj variable
-            obj[key] = value;
-
-            // now we have an object that we can now use that looks like this
-            // {
-            //     PENNY: 1.01,
-            //     NICKEL: 2.05,
-            //     DIME: 3.1,
-            //     QUARTER: 4.25,
-            //     ONE: 90,
-            //     FIVE: 55,
-            //     TEN: 20,
-            //     TWENTY: 60,
-            //     'ONE HUNDRED': 100
-            //   } 
-        }
+    // first we are going to create an object with the unit amounts
+    let obj = {
+        "PENNY": .01,
+        "NICKEL": .05,
+        "DIME": .10,
+        "QUARTER": .25,
+        "ONE": 1.00,
+        "FIVE": 5.00,
+        "TEN": 10.00,
+        "TWENTY": 20.00,
+        "ONE HUNDRED": 100.00
     }
-    // now we can find the totals for totals for the cash in the drawer
-        // Add up all the cash in the cid
-    // start by declaring a new variable and setting it to 0
+    // now we are going to create a variable that gives the total amount of cash in the drawer
     let totalCID = 0;
-    // now we are going to loop through the object
-    for (let key in obj){
-        // we are going to add each key value to the totalCID variable
-        totalCID += obj[key];
-        // noticed totalCID has long decimal, so we need to round it to 2 decimals
+    // next we are going to loop through the multidimensional array using a for of loop
+    // we are going to use a element of to get each element of the array or each array within the array
+    for (let element of cid){
+        // now we are going to take the 1st index or the second value of the array which is the cash amount and add it to our total 
+        totalCID += element[1];
     }
-    // rounding totalCID to 2 decimals
+    // I noticed that the totalCID variable contains multiple decimal points so we need to round it up to two
     totalCID = totalCID.toFixed(2);
-    // now we are going to create a variable amtDue variable that let's us know the change we need to give
+    // next we are going to create a new variable that will be the amount due
     let amtDue = cash - price;
-    
-    // first we are going to check if they gave us cash to cover the price
-    if (cash < price){
-        // then we are going to return an object that says status is insufficient funds
-        return {status: "INCORRECT_PAYMENT", change: []}
-    }
-   if (cash > price) {
-        // now we are going to check to see if the cash is equal to the total we have in the register
-        if (cash === totalCID){
-        // if it is then we are going to return an object that says status is closed
-            return {status: "CLOSED", change: []};
-        }
-    // now we are going to check if cash is less than the totalCID
-        if (cash < totalCID){
-        // then we are going to loop through the object to give them their change
-        // we actually we need to loop backwards so we can have go through the big bills first and then the change
-            for(let key in obj){
-            // while amount due is greater than  
-
-            }
-        }
-
-    }
-
+    // next we are going to create an empty array where we can push our currency values into for our change
+    let due = [];
 }
